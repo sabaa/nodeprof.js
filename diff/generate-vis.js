ca020a0a8c635e34cd81e7ed395f6b6b781f8117
sabaa <s.alimadadi@gmail.com>
2019-10-09 14:24:58 -0400

    update showing code differences for static/dynamic cg

--- diff/generate-vis.js
+++ diff/generate-vis.js
@@ -15,6 +15,8 @@ let projectName = 'acorn';
 let diffFilePath = path.join(__dirname, '..', 'test', 'output-actual', 'diff', projectName + '.json');
 
 let diffs = JSON.parse(fs.readFileSync(diffFilePath, 'utf8'));
+// diffs.files = [];
+let diffFiles = {};
 
 let displayedDiffs = [];
 
@@ -23,7 +25,6 @@ diffTable.innerHTML = '';
 for (let i = 0; i < 100/*diffs.length*/; i ++) {
     createDiffCell(i);
 
-    console.log('>>>>> ' + i);
     let diffCode = getCodeByLocation(diffs[i]);
 
     // console.log(diffCode);
@@ -55,27 +56,65 @@ diffDoc.head.appendChild(diffsScript);
 */
 fs.writeFileSync(diffVisPath, dom.serialize(), 'utf8');
 
-
 function getCode (filePath, startLine, startCol, endLine, endCol) {
-    let callerArray = fs.readFileSync(filePath, 'utf8').toString().split(eol);
+    let fileContent = fs.readFileSync(filePath, 'utf8').toString();
+    let callerArray = fileContent.split(eol);
+
+    if (typeof diffFiles[filePath] === 'undefined') {
+        diffFiles[filePath] = fileContent;
+    }
+    /*
+    let fileExists = false;
+    for (let f in diffFiles) {
+        if (diffFiles[f].filePath === filePath) {
+            fileExists = true;
+        }
+    }
+    if (!fileExists) {
+        diffFiles.push({
+            filePath: filePath,
+            content: fileContent
+        });
+        console.log(diffFiles.length);
+    }
+    */
+
     // let withinScopt = false;
     let code = '';
 
     let startLineItr = startLine;
     let startColItr = startCol;
 
+    let subStrStartIndex = -1, subStrEndIndex = -1;
+    let indexCounter = 0;
+
     for (let i = startLineItr; i <= endLine; i ++) {
         let line = callerArray[i].split('');
         for (let j = startColItr; j < line.length; j ++) {
+            if (i === startLine && j === startCol) {
+                subStrStartIndex = indexCounter;
+            }
+            if (i === endLine && j === endCol) {
+                subStrEndIndex = indexCounter;
+            }
+
             if (i === endLine && j > endCol) {
                 break;
             }
             code += line[j];
+
+            indexCounter ++;
         }
+        startColItr = 0;
+        code += eol;
+        indexCounter ++;
     }
 
-    console.log(code);
-    return code;
+    return {
+        code: code,
+        startIndex: subStrEndIndex,
+        endIndex: subStrEndIndex
+    };
 }
 
 function getCodeByLocation (diff) {
@@ -87,8 +126,9 @@ function getCodeByLocation (diff) {
     let callerEndCol = Number(diff.caller.end.column) - 1;
 
     let callsiteCode = getCode(callerPath, callerStartLine, callerStartCol, callerEndLine, callerEndCol);
-    diff.caller.code = callsiteCode;
-
+    diff.caller.code = callsiteCode.code;
+    diff.caller.startIndex = callsiteCode.startIndex;
+    diff.caller.endIndex = callsiteCode.endIndex;
 
     let calleePath = diff.callee.fullPath;
 
@@ -98,7 +138,9 @@ function getCodeByLocation (diff) {
     let calleeEndCol = Number(diff.callee.end.column) - 1;
 
     let calleeCode = getCode(calleePath, calleeStartLine, calleeStartCol, calleeEndLine, calleeEndCol);
-    diff.callee.code = calleeCode;
+    diff.callee.code = calleeCode.code;
+    diff.callee.startIndex = calleeCode.startIndex;
+    diff.callee.endIndex = calleeCode.endIndex;
 
     /*
     let callerArray = fs.readFileSync(callerPath, 'utf8').toString().split(eol);
@@ -159,5 +201,9 @@ function getCodeByLocation (diff) {
 
 
 let diffsForVis = 'let __diffs = ' + JSON.stringify(diffs) + ';';
+diffsForVis += eol;
+diffsForVis = diffsForVis + 'let __files = ' + JSON.stringify(diffFiles);
+// console.log(JSON.stringify(diffs, null, 2));
+// console.log('============== ' + diffs.files.length);
 let diffsForVisPath = path.join(__dirname, 'vis', 'data', projectName + '.js');
 fs.writeFileSync(diffsForVisPath, diffsForVis, 'utf8');
